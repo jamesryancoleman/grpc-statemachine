69d935777a584a244643714278c43090aa1ae712
James <32513142+jamesryancoleman@users.noreply.github.com>
2025-01-10 14:28:39 -0500

    updates to comms

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM python:1.13.1-slim-bookworm
+
+RUN mkdir -p /opt/bos/device/drivers/grpc-example
+
--- new-device.sh
+++ new-device.sh
@@ -6,7 +6,7 @@ echo "created dev:$DEVICE_ID"
 # add its points 
 PT1=$(mkd -p -n co2 -t brick:CO2_Sensor $DEVICE_ID)
 PT2=$(mkd -p -n air_temp -t brick:Air_Temperature_Sensor $DEVICE_ID)
-PT3=$(mkd -p -n air_temp_setpoint -t brick:Air_Temperature_Setpoint $DEVICE_ID)
+PT3=$(mkd -p -n air_temp_stpt -t brick:Air_Temperature_Setpoint $DEVICE_ID)
 PT4=$(mkd -p -n humidity -t brick:Humidity_Sensor $DEVICE_ID)
 PT5=$(mkd -p -n status -t brick:On_Off_Status $DEVICE_ID)
 PT6=$(mkd -p -n power_draw -t brick:Power_Sensor $DEVICE_ID)
