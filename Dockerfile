a46a8c273ac9808ef87ce3b62f2ee9d10877de98
James Coleman <jamesryancoleman@gmail.com>
2025-01-10 21:56:30 -0500

    updated dockerfile and server.py

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,14 @@
-FROM python:1.13.1-slim-bookworm
+FROM python:3.13.1-slim-bookworm
 
 RUN mkdir -p /opt/bos/device/drivers/grpc-example
 
+
+WORKDIR /opt/bos/device/drivers/grpc-example
+
+RUN pip install python-dateutil
+RUN python -m pip install grpcio
+RUN python -m pip install grpcio-tools
+
+COPY . /opt/bos/device/drivers/grpc-example/
+
+CMD ["python", "server.py"]
--- server.py
+++ server.py
@@ -202,7 +202,7 @@ if __name__ == "__main__":
         port = SERVE_PORT
         server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
         comms_pb2_grpc.add_GetSetRunServicer_to_server(GetSetRunServicer(), server)
-        server.add_insecure_port("[::]:" + port)
+        server.add_insecure_port("[::]" + port)
         server.start()
         print("Server started, listening on " + port)
         server.wait_for_termination()
